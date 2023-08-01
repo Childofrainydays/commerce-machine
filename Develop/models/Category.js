4e1e7fc59466dd011bb52ccfdd66c34911661e64
Childofrainydays <107083153+Childofrainydays@users.noreply.github.com>
2023-07-31 23:29:40 -0600

    added code for columns in Category,js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -12,11 +12,20 @@ Category.init(
   {
     // Define columns (also known as attributes) of the Category model
     // Specify the data type and any additional constraints for each column
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true, // Set the 'id' column to be the primary key
+      autoIncrement: true, // Set the 'id' column to auto-increment the primary key value each time a new record is added
+    },
+
+    category_name: {
+      type: DataTypes.STRING, // String to hold text
+      allowNull: false, // Set the 'category_name' column to not allow NULL values
+    }
   },
   {
-    // The first argument of 'init' is an object that describes the attributes of the model
     sequelize, // The Sequelize instance (connection) that the model should use
-    timestamps: false, // This option disables automatic timestamp fields 'createdAt' and 'updatedAt'
+    timestamps: false, 
     freezeTableName: true, // This option ensures that the table name remains the same as the model name ('category' in this case)
     underscored: true, // This option converts camelCase column names to snake_case in the database
     modelName: 'category', // This option sets the model name to 'category' (used for association methods)
